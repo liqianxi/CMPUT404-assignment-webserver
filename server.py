c871ebcb3e3400da9e7b8b49f26e356555458053
Qianxi Li <45400887+liqianxi@users.noreply.github.com>
2021-01-28 15:08:39 -0800

    add license

--- server.py
+++ server.py
@@ -19,6 +19,20 @@ import socketserver
 # some of the code is Copyright © 2001-2013 Python Software
 # Foundation; All Rights Reserved
 #
+#
+# Copyright 2021 Qianxi Li
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 # http://docs.python.org/2/library/socketserver.html
 #
 # run: python freetests.py
